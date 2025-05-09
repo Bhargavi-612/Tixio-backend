3451a5cd24a5f35dbb3f66861f4d8e3df4a6a312
Bhargavi-612 <bhargavi.acharya2018@gmail.com>
2025-05-10 03:59:38 +0530

    deployment errors

--- server.js
+++ server.js
@@ -23,6 +23,8 @@ app.use(express.json())
 app.use('/api/auth', authRoutes)
 app.use('/api/tickets', ticketRoutes)
 
+const PORT = process.env.PORT || 3000;
+
 // Connect to MongoDB
 mongoose.connect(process.env.MONGO_URI, {
   useNewUrlParser: true,
